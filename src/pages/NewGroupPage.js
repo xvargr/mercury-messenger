2d5cce2794077834b0166a19519c93458c36f650
xvargr <targenta650@gmail.com>
2022-08-02 21:50:19 +0800

    new forms and client side validation completed, todo http requests to backend api

--- src/pages/NewGroupPage.js
+++ src/pages/NewGroupPage.js
@@ -1,39 +1,14 @@
-// import ui
-import CardFloat from "../components/ui/CardFloat";
-import InputBox from "../components/ui/InputBox";
-import CircleButton from "../components/ui/CircleButton";
+import NewGroupForm from "../components/forms/NewGroupForm";
+
+const axios = require("axios").default;
 
 function NewGroupPage() {
-  return (
-    <div className="bg-gray-700 w-full flex justify-center items-center bgHeroDiagDark">
-      <CardFloat className="w-3/4 max-w-2xl">
-        <div className="text-mexican-red-600 mb-2 font-montserrat font-semibold">
-          New Group
-        </div>
-        <form className="flex flex-col">
-          <label className="mb-2 text-lg font-medium text-gray-900 dark:text-gray-400">
-            Group Name
-            <InputBox
-              type={"text"}
-              name={"group[name]"}
-              placeholder={"Name"}
-            ></InputBox>
-          </label>
-          <label className="mb-2 mt-1 text-lg font-medium text-gray-900 dark:text-gray-400">
-            Image
-            <InputBox
-              type={"file"}
-              name={"group[image]"}
-              id={"image"}
-            ></InputBox>
-          </label>
-          <div className="flex justify-end mt-2">
-            <CircleButton type={"submit"}></CircleButton>
-          </div>
-        </form>
-      </CardFloat>
-    </div>
-  );
+  function newGroupHandler(groupObject) {
+    fetch(); //! complete POST http request, axios/firebase
+    // todo: migrate to custom backend
+  }
+
+  return <NewGroupForm onNewGroup={newGroupHandler} />;
 }
 
 export default NewGroupPage;
