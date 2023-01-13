9c6a3ebdc3e9826d62e778733d138808ad97c2d9
xvargr <targenta650@gmail.com>
2023-01-13 16:35:10 +0800

    restructure, serve static react app

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+
+import "./reset.css";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+); // render <App> in #root element
